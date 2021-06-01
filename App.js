e7cfaa0804f78060e4ffcf6e25af0df89faa8736
callas4ever <thetseringnorbu@gmail.com>
2021-06-01 11:28:33 -0400

    setting up the skeleton

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Kuuco from './src/index'
 export default function App() {
   return (
     <View style={styles.container}>
+      <Kuuco/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+const Kuuco=()=>{
+    return(
+        <view>
+            <Text>
+                Hi from the landing page!
+            </Text>
+        </view>
+    )
+}
+export default Kuuco
